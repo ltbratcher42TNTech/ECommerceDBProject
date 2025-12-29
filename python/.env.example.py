ffe1badfe1fed69e7fba5ec927e3080243267e43
ltbratcher42TNTech <ltbratcher42@tntech.edu>
2025-12-29 00:42:00 -0600

    Create .env.example.py for database settings
    Added example .env file for database configuration.

---
+++ python/.env.example.py
@@ -0,0 +1,10 @@
+# you will need a file. This file will be named .env, and  you need it to utilize config.py. 
+# I utilized .env to shield my sensitive database information like passwords and similar to
+# demonstrate data integrity, as one would do in the industry. 
+
+# Below are the contents, copy and paste into a file named ".env" and fill with your information
+
+DB_HOST=localhost
+DB_USER=your_username        # this is usually root
+DB_PASSWORD=your_password    # This is the password used to run your database server. In my instance, MariaDB
+DB_NAME=ecommerce_db
